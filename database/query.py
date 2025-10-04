869e71e874eabd67f94ac50c9301feaca1e965fb
hnaveed23nav <hnaveed@stac.edu>
2025-10-04 11:55:31 -0400

    feat: update query and add restaurant dataset

--- database/query.py
+++ database/query.py
@@ -1,3 +1,11 @@
-def get_restaurant_by_name(name):
-    print(f"Searching for restaurant: {name}")
+import pandas as pd
 
+data = pd.read_csv('database/DOHMH_New_York_City_Restaurant_Inspection_Results_20251004.csv')
+
+def get_restaurant_by_name(name):
+    result = data[data['DBA'].str.contains(name, case=False, na=False)]
+    print(result[['DBA', 'BORO', 'BUILDING', 'STREET', 'CUISINE DESCRIPTION', 'GRADE', 'VIOLATION DESCRIPTION', 'INSPECTION DATE']].head())
+
+if __name__ == "__main__":
+    name = input("Enter restaurant name: ")
+    get_restaurant_by_name(name)
