0157591d5b1affef744c2c73aa3feba8268a918a
TamaraEd23 <Tamaraed6@gmail.com>
2025-11-11 19:51:23 -0500

    Center About page content and fix contact section horizontal layout

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
-import Home from './Home'
+import './styles.css'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/explore" element={<App />} />
-            </Routes>
-        </BrowserRouter>
+        <App />
     </React.StrictMode>
 )
 
